137e59853ca1244ae774ed8239352a86ce2dcb73
Ged Addams <gedaddamsdevelopment@gmail.com>
2021-10-16 10:44:05 +0200

    Changed create_sync_lists to create_sync_sets which now of course returns sets instead of lists

--- sync_functions.py
+++ sync_functions.py
@@ -22,10 +22,10 @@ def sync(source, target, delete, dry_run, verbose):
     target_files = create_file_dict(target)
     logger.debug(f"Time for create_file_dict 2 {round(time() - time_point, 2)}")
     time_point = time()
-    lr_list, rl_list, del_src, del_tar = create_sync_lists(source, target, source_files, target_files)
-    logger.debug(f"Time for create_sync_lists {round(time() - time_point, 2)}")
+    lr_list, rl_list, del_src, del_tar = create_sync_sets(source, target, source_files, target_files)
+    logger.debug(f"Time for create_sync_sets {round(time() - time_point, 2)}")
     time_point = time()
-    #logger.debug(f"lr list: {lr_list}\nrl list: {rl_list}\ndel src list: {del_src}\ndel tar list: {del_tar}")
+    logger.debug(f"lr list: {lr_list}\nrl list: {rl_list}\ndel src list: {del_src}\ndel tar list: {del_tar}")
 
     # Returns if all relevant lists are empty!
     if not lr_list and not rl_list:
@@ -36,13 +36,16 @@ def sync(source, target, delete, dry_run, verbose):
             return
     
     script_dir = os.path.dirname(os.path.realpath(__file__))
-    lr_filepath = os.path.join(script_dir, "lr_sync.tmp")
-    rl_filepath = os.path.join(script_dir, "rl_sync.tmp")
-    
-    with open(lr_filepath, 'w') as file_lr:
-        file_lr.writelines([line + '\n' for line in lr_list])
-    with open(rl_filepath, 'w') as file_rl:
-        file_rl.writelines([line + '\n' for line in rl_list])
+
+    if lr_list:
+        lr_filepath = os.path.join(script_dir, "lr_sync.tmp")
+        with open(lr_filepath, 'w') as file_lr:
+            file_lr.writelines([line + '\n' for line in lr_list])
+
+    if rl_list:
+        rl_filepath = os.path.join(script_dir, "rl_sync.tmp")
+        with open(rl_filepath, 'w') as file_rl:
+            file_rl.writelines([line + '\n' for line in rl_list])
         
     # TODO. Here happens the actual syncing.
     #return_lr = rsync(source, target, delete, dry_run, verbose, False, True, lr_filepath)
@@ -96,32 +99,33 @@ def item_existed(a_file):
     return False
 
 
-def create_sync_lists(source, target, src_files, tar_files):
+def create_sync_sets(source, target, src_files, tar_files):
     # TODO Figure out how to handle if there exist dir on one side with same
     # path as file on other side. Maybe always append dir even if have files
     # and do some kind of set union?
-    def add_or_delete_item(a_file, add_list, del_list):
+    def add_or_delete_item(a_file, add_set, del_set):
         if item_existed(a_file):
-            del_list.append(a_file)
+            del_set.add(a_file)
         else:
-            add_list.append(a_file)
+            add_set.add(a_file)
 
-    def add_or_delete_folder(root_path, dict_of_files_in_root, add_list, del_list):
+    def add_or_delete_folder(root_path, dict_of_files_in_root, add_set, del_set):
         # Checks if folder existed in previous sync
         if item_existed(root_path):
-            choosen_list = del_list
+            choosen_set = del_set
         else:
-            choosen_list = add_list
+            choosen_set = add_set
 
         # If no files in dir append dir itself.
         if not dict_of_files_in_root:
-            choosen_list.append(root_path)
+            choosen_set.add(root_path)
         else:
             # If there are files in dir append the files.
             for item in dict_of_files_in_root:
-                choosen_list.append(item)
+                choosen_set.add(item)
 
-    left_to_right, right_to_left, delete_list_src, delete_list_tar = [], [], [], []
+    left_to_right, right_to_left, delete_set_src, delete_set_tar = set(), set(), set(), set()
+
     # Loop through all keys in src_files. Root corresponds to existing dirs
     for root in src_files:
         basedir_src = src_files[root]
@@ -140,31 +144,31 @@ def create_sync_lists(source, target, src_files, tar_files):
 
                     # TODO floating point arithmetics might cause errors. Rethink.
                     if date_diff > 0:
-                        left_to_right.append(item)
+                        left_to_right.add(item)
                     elif date_diff < 0:
-                        right_to_left.append(item)
+                        right_to_left.add(item)
 
                     # Delete key from target dict
                     basedir_tar.remove(item)
                 else: 
                     # File only in source not in target
-                    add_or_delete_item(item, left_to_right, delete_list_src)
+                    add_or_delete_item(item, left_to_right, delete_set_src)
                     
             # Checks if there are files exclusive to target (not in source)
             for item in basedir_tar:
-                add_or_delete_item(item, right_to_left, delete_list_tar)
+                add_or_delete_item(item, right_to_left, delete_set_tar)
             # delete key corresponding to root from tar_files
             del tar_files[root]
 
         else:
             # root exists in source but not in target
-            add_or_delete_folder(root, basedir_src, left_to_right, delete_list_src)
+            add_or_delete_folder(root, basedir_src, left_to_right, delete_set_src)
 
     # Only remaining roots in tar_files exists only in target (not in source).
     for root in tar_files:
-        add_or_delete_folder(root, tar_files[root], right_to_left, delete_list_tar)
+        add_or_delete_folder(root, tar_files[root], right_to_left, delete_set_tar)
 
-    return left_to_right, right_to_left, delete_list_src, delete_list_tar
+    return left_to_right, right_to_left, delete_set_src, delete_set_tar
     
 
 def format_rsync_output(st_ouput):
