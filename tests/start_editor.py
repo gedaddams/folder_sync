a139a715c99dbe752ba2416947a150e60dbbf53f
Ged Addams <gedaddamsdevelopment@gmail.com>
2021-10-24 23:25:30 +0200

    More training

--- tests/start_editor.py
+++ tests/start_editor.py
@@ -2,17 +2,18 @@ import os
 import pathlib
 import subprocess
 
-# Below doesnt work
-#arglist = ["vim", pathlib.Path.home() / "Documents/testfile.txt"]
-#obj_return = subprocess.run(arg_string, shell=True, text=True, capture_output=True)
-
 file_path = pathlib.Path.home() / "Documents/testfile.txt"
-arg_string = f"vim {file_path}"
 
-os.system(f"vim {file_path}")
+arglist = ["vim", file_path]
+obj_return = subprocess.run(arglist)
+print(obj_return.returncode)
+
+#arg_string = f"vim {file_path}"
+#os.system(f"vim {file_path}")
 
 with file_path.open("r") as f:
-    text = f.readlines()
-output = "".join(text)
-print()
-print(output)
+    text = [line.split(maxsplit=1) for line in f.readlines()]
+
+for item in text:
+    print(f"Column 1: {item[0]}")
+    print(f"Column 2: {item[1]}")
